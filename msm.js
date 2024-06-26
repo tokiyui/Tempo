35fed029f4d50726555e6b5bf2d753c3d8ce9c03
Horita <ryohorita105@gmail.com>
2024-06-26 18:22:51 +0900

    Update msm.js

--- msm.js
+++ msm.js
@@ -206,7 +206,6 @@ function update() {
         $("#selectRegion").prop("disabled", false);
         $("#selectPref").prop("disabled", false);
         $("#selectPoint").prop("disabled", false);
-    });
 }
 
 function drawTable() {
